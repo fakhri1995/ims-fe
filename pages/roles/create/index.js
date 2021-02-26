945ecbd55cb227c2c84b7f00a5f8eb2c0b653afd
Yusuf-MIG <yusuf@mitrasolusi.group>
2021-02-26 13:59:35 +0700

    create roles page

--- pages/roles/create/index.js
+++ pages/roles/create/index.js
@@ -1,144 +1,170 @@
 import Layout from '../../../components/layout-dashboard'
 import httpcookie from 'cookie'
 import { useRouter } from 'next/router'
-import Table from 'antd/lib/table'
-import Tooltip from 'antd/lib/tooltip'
-import Button from 'antd/lib/button'
-import Drawer from 'antd/lib/drawer'
-import CopyOutlined from '@ant-design/icons/CopyOutlined'
 import { useState } from 'react'
 import Link from 'next/link'
 import Sticky from 'wil-react-sticky'
 import st from '../../../components/layout-dashboard.module.css'
-
+import Form from 'antd/lib/form'
+import { Input, Tabs, Tree } from 'antd'
+import Divider from 'antd/lib/divider'
 
 function RolesCreate({ initProps, dataProfile, sidemenu }) {
     const rt = useRouter()
     const tok = initProps
     const pathArr = rt.pathname.split("/").slice(1)
     const { originPath } = rt.query
+    const { TextArea } = Input;
+    const { TabPane } = Tabs;
 
-    const [drawablecreate, setDrawablecreate] = useState(false)
-
-    const columnsDD = [
+    //----------CreateGroup-------------
+    const [newroles, setNewroles] = useState({
+        name: '',
+        description: ''
+    })
+    const onChangeCreateGroup = (e) => {
+        var val = e.target.value
+        setNewroles({
+            ...newgroup,
+            [e.target.name]: val
+        })
+    }
+    const treeData = [
         {
-            title: 'role',
-            dataIndex: 'name',
-            key: 'role',
-            render(text, record) {
-                return {
-                    props: {
-                    style: { background: record.key%2 == 1 ? '#f2f2f2' : '#fff' },
-                  },
-                    children: <div>{text}</div>,
-                };
-            },
-            // sorter: (a, b) => a.user_id - b.user_id,
-            // sortDirections: ['descend', 'ascend'],
-        },
-        {
-            title: 'nama',
-            dataIndex: 'agent',
-            key: 'nama',
-            render(text, record) {
-                return {
-                    props: {
-                        style: { background: record.key%2 == 1 ? '#f2f2f2' : '#fff' },
-                    },
-                    children: <div>{text}</div>,
-                };
-            },
-            // render: agent => (
-
-            //   ),
-            // sorter: (a, b) => a.fullname.localeCompare(b.fullname),
-            // sortDirections: ['descend', 'ascend'],
-            // responsive: ['lg'],
-        },
-        {
-            title: 'action', // Non-breakable space is char 0xa0 (160 dec)
-            dataIndex: 'actionss',
-            key: 'action',
-            render: (text, record, index) => {
-                return {
-                    props: {
-                        style: { background: record.key%2 == 1 ? '#f2f2f2' : '#fff' },
-                    },
-                    children: <Tooltip placement="topLeft" title={"Clone"}>
-                    {/* {actions[index]} */}
-                    <Button>
-                        <Link href={{
-                            pathname: `/clone/${record.key}`,
-                            query: {
-                                originPath: 'Admin'
-                            }
-                        }}><a><CopyOutlined /></a></Link>
-                    </Button>
-                </Tooltip>
-                }
-            }
-        }
-    ];
-
-    const data = [
-        {
-            key: '1',
-            name: 'Account Admin',
-            agent: '4 Agents',
-            actionss: 'clone'
-        },
-        {
-            key: '2',
-            name: 'Admin',
-            agent: 'No Agents',
-            actionss: 'clone'
-        },
-        {
-            key: '3',
-            name: 'SD Supervisor',
-            agent: 'No Agents',
-            actionss: 'clone'
+            title: 'parent 1',
+            key: '0-0',
+            children: [
+                {
+                    title: 'parent 1-0',
+                    key: '0-0-0',
+                    // disabled: true,
+                    children: [
+                        {
+                            title: 'leaf',
+                            key: '0-0-0-0',
+                            // disableCheckbox: true,
+                        },
+                        {
+                            title: 'leaf',
+                            key: '0-0-0-1',
+                        },
+                    ],
+                },
+                {
+                    title: 'parent 1-1',
+                    key: '0-0-1',
+                    children: [
+                        {
+                            title: (
+                                <span
+                                    style={{
+                                        color: '#1890ff',
+                                    }}
+                                >
+                                    sss
+                                </span>
+                            ),
+                            key: '0-0-1-0',
+                        },
+                    ],
+                },
+            ],
         },
     ];
+    const onSelect = (selectedKeys, info) => {
+        console.log('selected', selectedKeys, info);
+    };
 
+    const onCheck = (checkedKeys, info) => {
+        console.log('onCheck', checkedKeys, info);
+    };
     return (
         <Layout tok={tok} dataProfile={dataProfile} pathArr={pathArr} sidemenu={sidemenu} originPath={originPath} st={st}>
             <>
                 <div className="w-full h-auto grid grid-cols-1 md:grid-cols-4">
-                    <div className=" col-span-1 md:col-span-3 flex flex-col" id="formAgentsWrapper">
-                        <Sticky containerSelectorFocus="#formAgentsWrapper">
-                            <div className="flex justify-between p-4 border-gray-400 border-t border-b bg-white mb-8">
-                                <h1 className="font-semibold text-base w-auto py-2">Roles</h1>
-                                <div className="flex space-x-2">
-                                    <div className=" text-white text-sm bg-gray-700 hover:bg-gray-900 cursor-pointer rounded-md h-10 py-2 w-32 text-center" onClick={() => { setDrawablecreate(true) }}>New Role</div>
+                    <Form layout="vertical" style={{ display: 'contents' }}>
+                        <div className=" col-span-1 md:col-span-3 flex flex-col" id="formAgentsWrapper">
+                            <Sticky containerSelectorFocus="#formAgentsWrapper">
+                                <div className="flex justify-between p-4 border-gray-400 border-t border-b bg-white mb-8">
+                                    <h1 className="font-semibold text-base w-auto py-2">New Roles</h1>
+                                    <div className="flex space-x-2">
+                                        <Link href="/groups?originPath=Admin" >
+                                            <div className=" text-black text-sm bg-white hover:bg-gray-300 border-2 border-gray-900 cursor-pointer rounded-md h-10 py-2 w-20 text-center" >
+                                                <p>
+                                                    Cancel
+                                        </p>
+                                            </div>
+                                        </Link>
+                                        <button type="submit" className=" text-white text-sm bg-gray-700 hover:bg-gray-900 cursor-pointer rounded-md h-10 py-2 w-20 text-center" >
+                                            <p>Save</p>
+                                        </button>
+                                    </div>
                                 </div>
+                            </Sticky>
+
+                            {/* <div className="w-120 h-auto p-0 "> */}
+                            <div className="pb-4 md:mb-0 ">
+                                <Form.Item name="name" style={{ marginRight: `1rem` }} label="Name"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Nama role harus diisi',
+                                        },
+                                    ]}
+                                    initialValue={newroles.name}
+                                >
+                                    <Input placeholder="Group Name" name={`name`} onChange={onChangeCreateGroup}></Input>
+                                </Form.Item>
                             </div>
-                        </Sticky>
 
-                        <div className="col-span-3 flex flex-col space-y-3">
+                            <div className="pb-4 md:mb-0">
+                                <Form.Item name="description" style={{ marginRight: `1rem` }} label="Description"
+                                    rules={[
+                                        {
+                                            required: true,
+                                            message: 'Deskripsi role harus diisi',
+                                        },
+                                    ]}
+                                    initialValue={newroles.description}
+                                >
+                                    <TextArea placeholder="Group Description" rows={2} name={`description`} onChange={onChangeCreateGroup} />
+                                </Form.Item>
+                            </div>
 
-                            <Table showHeader={false} scroll={{ x: 400 }} dataSource={data} columns={columnsDD} onRow={(record, rowIndex) => {
-                                // return {
-                                //     onMouseOver: (event) => {
-                                //         var actionsCopy = actions
-                                //         actionsCopy[rowIndex] = true
-                                //         setActions(actionsCopy)
-                                //         setAction("block")
-                                //         // console.log("row: " + actions[rowIndex] + " " + rowIndex)
-                                //     },
-                                //     onMouseLeave: (event) => {
-                                //         var actionsCopy = actions
-                                //         actionsCopy[rowIndex] = false
-                                //         setActions(actionsCopy)
-                                //         setAction("hidden")
-                                //         // console.log("row leave: " + actions[rowIndex] + " " + rowIndex)
-                                //     }
-                                // }
-                            }}></Table>
+                            {/* </div> */}
+                            <Divider style={{ borderTop: '1px solid rgba(0, 0, 0, 0.2)' }} />
+                            <h1 className="font-semibold text-base w-auto p-2">Permissions</h1>
+                            {/* <div className="border-gray-300 p-4 border bg-white w-full h-auto "> */}
+                            {/* <Tabs defaultActiveKey="1" tabPosition={'left'} style={{ }}>
+                                {[...Array.from({ length: 10 }, (v, i) => i)].map(i => (
+                                <TabPane tab={`Tab-${i}`} key={i} disabled={i === 5}>
+                                    Content of tab {i}
+                                </TabPane>
+                                ))}
+                            </Tabs> */}
+                            <Tabs defaultActiveKey="1" tabPosition={'left'} style={{}}>
+                                <TabPane tab={`Ticket`} key={1} >
+                                    <Tree
+                                        checkStrictly={true}
+                                        checkable
+                                        defaultExpandedKeys={['0-0-0', '0-0-1']}
+                                        defaultSelectedKeys={['0-0-0', '0-0-1']}
+                                        defaultCheckedKeys={['0-0-0', '0-0-1']}
+                                        onSelect={onSelect}
+                                        onCheck={onCheck}
+                                        treeData={treeData}
+                                    />
+                                </TabPane>
+                                <TabPane tab={`Problems`} key={2} >
+                                    Content of tab 2
+                                </TabPane>
+                                <TabPane tab={`Changes`} key={3} >
+                                    Content of tab 3
+                                </TabPane>
+                            </Tabs>
+                            {/* </div> */}
                         </div>
-
-
-                    </div>
+                    </Form>
                     <div className="flex flex-col space-y-3 px-4">
                         <div className="font-semibold text-sm">Understanding Roles</div>
                         <p className="font-normal text-sm">
@@ -157,20 +183,6 @@ function RolesCreate({ initProps, dataProfile, sidemenu }) {
                     </p>
                     </div>
                 </div>
-
-                <Drawer title="New Role" maskClosable={false} visible={drawablecreate} onClose={() => { setDrawablecreate(false) }} width={720} footer={
-                    <div style={{ textAlign: 'right' }}>
-                        <button onClick={() => { setDrawablecreate(false) }} className="bg-white-700 hover:bg-gray-300 border text-black py-1 px-2 rounded-md w-20 mr-4">
-                            Cancel
-                        </button>
-                        <button type="primary" className="bg-blue-700 hover:bg-blue-800 border text-white py-1 px-2 rounded-md w-20">
-                            Submit
-                        </button>
-                    </div>
-                }>
-                </Drawer>
-
-
             </>
         </Layout>
     )
