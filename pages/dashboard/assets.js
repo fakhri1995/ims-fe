f01d5f9d8b556376efecbecd4748a2500f0dc54e
yuesmitrasolusi <yues@mitrasolusi.group>
2021-02-27 12:52:37 +0700

    merged from yusuf

--- pages/dashboard/assets.js
+++ pages/dashboard/assets.js
@@ -1,12 +1,16 @@
 import { useRouter } from 'next/router'
 import httpcookie from 'cookie'
+import Tree from 'antd/lib/tree'
 import Layout from '../../components/layout-dashboard-main'
 import st from "../../components/layout-dashboard-main.module.css"
+import { Children } from 'react'
 // import TeamOutlined from '@ant-design/icons/TeamOutlined'
 // import UserOutlined from '@ant-design/icons/UserOutlined'
 // import Link from 'next/link'
 
-function Assets({ initProps, dataProfile, sidemenu }) {
+function Assets({ initProps, dataProfile, sidemenu, dataAssetsList }) {
+    console.log(dataAssetsList.data)
+
     const rt = useRouter()
     const tok = initProps
     const pathArr = rt.pathname.split("/").slice(1)
@@ -15,7 +19,7 @@ function Assets({ initProps, dataProfile, sidemenu }) {
             <div className="w-full h-auto border-t border-opacity-30 border-gray-500 bg-white">
                 <div className="grid grid-cols-1 md:grid-cols-4">
                     <div className="col-span-1 md:col-span-3 flex flex-col">
-                        <div className="p-2 md:p-5 border-b flex justify-between">
+                        <div className="p-2 md:p-5 mb-5 border-b flex justify-between">
                             <div className="text-xs md:text-sm font-semibold">
                                 <h1 className="mt-2">Assets Types & Fields</h1>
                             </div>
@@ -23,12 +27,43 @@ function Assets({ initProps, dataProfile, sidemenu }) {
                                 New Asset Type
                             </div>
                         </div>
+                        <div className="p-2 md:p-5">
+                            {/* <Tree
+                                onExpand={onExpand}
+                                expandedKeys={expandedKeys}
+                                autoExpandParent={autoExpandParent}
+                                onSelect={onSelect}
+                                selectedKeys={selectedtree}
+                                treeData={treeData}
+                                selectable
+                                titleRender={(nodeData) => (
+                                    <>
+                                        <Link href={`/company/locations/update/${dataDetailCompany.data}?originPath=Admin`}>
+                                            <div className={`flex justify-between hover:bg-blue-100 text-black`}
+                                                onMouseOver={() => {
+                                                    setHoverrowtree("flex");
+                                                }}
+                                                onMouseLeave={() => {
+                                                    setHoverrowtree("hidden");
+                                                }}
+                                            >
+                                                <div className="mr-20">
+                                                    {nodeData.title}
+                                                </div>
+                                            </div>
+                                        </Link>
+                                    </>
+                                )
+                                }
+                                blockNode={true}
+                            /> */}
+                        </div>
                     </div>
                     <div className="col-span-1 md:col-span-1 flex flex-col p-2 md:p-5">
                         <h1 className="text-xs md:text-sm font-semibold mb-5">Asset Types & Fields</h1>
                         <p className="text-xs md:text-sm">
-                        Freshservice lets you maintain a repository of assets by creating a structure of configuration types in your help desk. You can add asset types at the root level or within another asset type, and have several items mapped to those types.You can also disable the default asset types so that only the ones you need are visible.
-                        <br/><br/>
+                            Freshservice lets you maintain a repository of assets by creating a structure of configuration types in your help desk. You can add asset types at the root level or within another asset type, and have several items mapped to those types.You can also disable the default asset types so that only the ones you need are visible.
+                        <br /><br />
                         When you open an asset, you can find out whether it is currently being used, its business impact and the employee it’s assigned to in your team. In addition you will also be able to pull out specifications, relationship details etc. about the asset without switching between different pages.
                         </p>
                     </div>
@@ -60,10 +95,21 @@ export async function getServerSideProps({ req, res }) {
     })
     const resjson = await resources.json()
     const dataProfile = resjson
+
+    const resourcesGA = await fetch(`https://boiling-thicket-46501.herokuapp.com/getAssets`, {
+        method: `GET`,
+        headers: {
+            'Authorization': JSON.parse(initProps),
+        }
+    })
+    const resjsonGA = await resourcesGA.json()
+    const dataAssetsList = resjsonGA
+
     return {
         props: {
             initProps,
             dataProfile,
+            dataAssetsList,
             sidemenu: "3"
         },
     }
