8e959478b80ffb8da55ecceac06e63c1de584619
Yusuf-MIG <yusuf@mitrasolusi.group>
2021-04-13 19:06:28 +0700

    section 1

--- components/migwebsite/styles.js
+++ components/migwebsite/styles.js
@@ -1,14 +1,68 @@
 // File components/style.js
 
 import React from 'react'
-
+// font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
+//             Helvetica, sans-serif;
 const Styles = (props) => (
   <div>
       <style jsx global>{`
+      .row:after { clear: both; }
+
+      .row:before, .row:after {
+        content: " ";
+        display: table; }
+      
+      @media (min-width: 768px) {
+        .col { 
+          float: left;
+          width: 33.33333333%; }
+      
+        .alignImgRight { text-align: right; }
+      }
+      .header{
+        padding: 0 50px;
+      }
+      @media (max-width: 540px){
+        .header{
+          padding: 0 25px;
+        }
+      }
+      h1 {
+        display: block;
+        font-size: 3em;
+        margin-top: 0.67em;
+        margin-bottom: 0.67em;
+        margin-left: 0;
+        margin-right: 0;
+        font-weight: bold;
+      }
+      h4 {
+        display: block;
+        font-size: 1em;
+        margin-top: 1.33em;
+        margin-bottom: 1.33em;
+        margin-left: 0;
+        margin-right: 0;
+        font-weight: bold;
+      }
+        .center {
+          margin: auto;
+          width: 90%;
+          padding: 10px;
+        }
+        .menuToggle:checked + .menu {
+          display: grid;
+          margin: 1.5rem 0rem;
+          padding: 1rem 0rem;
+          background: white;
+          border: 1px;
+          width: -webkit-fill-available;
+        }
+        
         body {
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-            Helvetica, sans-serif;
+          
+          font-family: Raleway;
         }
         .hero {
           width: 100%;
--- pages/migwebsite/landingpage/index.js
+++ pages/migwebsite/landingpage/index.js
@@ -3,14 +3,63 @@
 import React from 'react'
 import Link from 'next/link'
 import Layout from '../../../components/migwebsite/layout.js'
+import {Typography, Space, Button,Row,Col } from 'antd'
 
 function LandingPage({}) {
+    const { Title } = Typography;
     // const tok = initProps
     // const cook = jscookie.get('token')
     // console.log("cookie di dashboard: " + cook)
     return (
         <Layout>
-            <h1>Selamat datang di Dashboard</h1>
+            <div className={'section1'}>
+                {/* <div className={'center'}>
+                    <Title className={''}>Your One Stop,<br></br>
+                        Cost Efficient IT Solutions
+                    </Title>
+                    <Title level={4}>Improving efficiencies by supporting you with staff <br></br>
+                        augmentation, software and hardware managed services.
+                    </Title>
+                </div>
+                <div className="top-3 " >
+                        <img width={'auto'} height={'auto'} src='/image-section1.png'/>
+                    </div> */}
+                    {/* <div className="space-align-container">
+                        <div className="space-align-block">
+                        <Space size={10} align="center">
+                        <div className={''}>
+                            <Title className={''}>Your One Stop,<br></br>
+                                Cost Efficient IT Solutions
+                            </Title>
+                            <Title level={4}>Improving efficiencies by supporting you with staff <br></br>
+                                augmentation, software and hardware managed services.
+                            </Title>
+                        </div>
+                            <span className="mock-block">
+                                <img style={{minWidth:'400px', height:'auto'}} src='/image-section1.png'/>
+                            </span>
+                        </Space>
+                        </div>
+                    </div> */}
+            </div>
+            <div className={'section2'}>
+                    <div className={'block md:flex'}>
+                        <div className={'flex-col center'}>
+                            <div className={''}>
+                                <p className={'text-3xl md:text-4xl'} style={{fontWeight: 'bold'}}>Your One Stop,
+                                    Cost Efficient IT Solutions
+                                </p>
+                                <p className={' text-sm md:text-base font-bold'}>Improving efficiencies by supporting you with staff 
+                                    augmentation, software and hardware managed services.
+                                </p>
+                            </div>
+                        </div>
+                        <div className={'flex-col'}>
+                            <img style={{width:'1500px', height:'auto'}} src='/image-section1.png'/>
+                        </div>
+                    </div>
+            </div>
+
         </Layout>
     )
 }
