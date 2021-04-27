48ca8fe83161c1f969b95934f1bfabc02469628c
yuesmitrasolusi <yues@mitrasolusi.group>
2021-04-27 16:23:19 +0700

    API in agent groups

--- pages/groups/create/agents/index.js
+++ pages/groups/create/agents/index.js
@@ -5,7 +5,7 @@ import { useState } from 'react'
 import Link from 'next/link'
 import Sticky from 'wil-react-sticky'
 import st from '../../../../components/layout-dashboard-groups.module.css'
-import {Divider,Input,Select,Radio,Row,Col,Button,notification,Form} from 'antd'
+import { Divider, Input, Select, Radio, Row, Col, Button, notification, Form } from 'antd'
 
 function GroupsAgentsCreate({ initProps, dataProfile, dataListAccount, sidemenu }) {
     const rt = useRouter()
@@ -21,7 +21,6 @@ function GroupsAgentsCreate({ initProps, dataProfile, dataListAccount, sidemenu 
         name: '',
         description: '',
         group_head: dataProfile.data.user_id,
-        is_agent: true,
         user_ids: []
     })
     const onChangeCreateGroup = (e) => {
@@ -52,7 +51,7 @@ function GroupsAgentsCreate({ initProps, dataProfile, dataListAccount, sidemenu 
     const handleCreateGroup = () => {
         // console.log(newgroup)
         setLoadingbtn(true)
-        fetch(`https://boiling-thicket-46501.herokuapp.com/addGroup`, {
+        fetch(`https://boiling-thicket-46501.herokuapp.com/addAgentGroup`, {
             method: 'POST',
             headers: {
                 'Authorization': JSON.parse(tok),
@@ -75,7 +74,6 @@ function GroupsAgentsCreate({ initProps, dataProfile, dataListAccount, sidemenu 
                 else if (!res2.success) {
                     notification['error']({
                         message: res2.message.errorInfo[3],
-                        // message: res2.message,
                         duration: 3
                     })
                 }
@@ -92,7 +90,7 @@ function GroupsAgentsCreate({ initProps, dataProfile, dataListAccount, sidemenu 
     //------------------------------------------
 
     //------------populate list account-------------
-    const dataDD = dataListAccount.data.data.accounts.map((doc, idx) => {
+    const dataDD = dataListAccount.data.map((doc, idx) => {
         return ({
             value: doc.user_id,
             label: doc.fullname,
@@ -105,30 +103,22 @@ function GroupsAgentsCreate({ initProps, dataProfile, dataListAccount, sidemenu 
     return (
         <Layout tok={tok} dataProfile={dataProfile} pathArr={pathArr} sidemenu={sidemenu} originPath={originPath} dataDetailGroup={dataDetailGroup} st={st}>
             <>
-                <div className="w-full h-auto grid grid-cols-1 md:grid-cols-4">
-                    <Form layout="vertical" onFinish={handleCreateGroup} style={{ display: 'contents' }} form={instanceForm}>
-                        <div className=" col-span-1 md:col-span-3 flex flex-col" id="formAgentsWrapper">
-                            <Sticky containerSelectorFocus="#formAgentsWrapper">
-                                <div className="flex justify-between p-4 border-gray-400 border-t border-b bg-white mb-8">
-                                    <h1 className="font-semibold text-base w-auto">Group Agents Baru</h1>
-                                    <div className="flex space-x-2">
-                                        <Link href="/groups?originPath=Admin" >
-                                            <Button type="default" size="middle">Batalkan</Button>
-                                            {/* <div className=" text-black text-sm bg-white hover:bg-gray-300 border-2 border-gray-900 cursor-pointer rounded-md h-10 py-2 w-20 text-center" >
-                                        <p>
-                                        Cancel
-                                        </p>
-                                    </div> */}
-                                        </Link>
-                                        <Button type="primary" size="middle" onClick={instanceForm.submit} loading={loadingbtn}>Simpan</Button>
-                                        {/* <button type="submit" className=" text-white text-sm bg-gray-700 hover:bg-gray-900 cursor-pointer rounded-md h-10 py-2 w-20 text-center" >
-                                        <p>Save</p>
-                                    </button> */}
-                                    </div>
+                <div className="w-full h-auto grid grid-cols-1 md:grid-cols-4" id="formAgentsWrapper">
+                    <div className="col-span-1 md:col-span-4">
+                        <Sticky containerSelectorFocus="#formAgentsWrapper">
+                            <div className="flex justify-between p-4 border-gray-400 border-t border-b bg-white mb-8">
+                                <h1 className="font-semibold text-base w-auto">Group Agents Baru</h1>
+                                <div className="flex space-x-2">
+                                    <Link href="/groups?originPath=Admin" >
+                                        <Button type="default" size="middle">Batalkan</Button>
+                                    </Link>
+                                    <Button type="primary" size="middle" onClick={instanceForm.submit} loading={loadingbtn}>Simpan</Button>
                                 </div>
-                            </Sticky>
-
-                            {/* <div className="w-120 h-auto p-0 "> */}
+                            </div>
+                        </Sticky>
+                    </div>
+                    <Form layout="vertical" onFinish={handleCreateGroup} style={{ display: 'contents' }} form={instanceForm}>
+                        <div className=" col-span-1 md:col-span-3 flex flex-col">
                             <div className="pb-4 md:mb-0 ">
                                 <Form.Item name="name" style={{ marginRight: `1rem` }} label="Group Name"
                                     rules={[
@@ -185,13 +175,13 @@ function GroupsAgentsCreate({ initProps, dataProfile, dataListAccount, sidemenu 
                                 </Radio.Group>
                                 <Row>
                                     <Col flex="auto">
-                                        <Select 
-                                        showSearch placeholder="Add an Agent" 
-                                        showArrow mode="multiple"  
-                                        onChange={handleChangeAddAgent} 
-                                        style={{ width: '100%', padding: '0 5px', lineHeight: '2.4' }} 
-                                        optionFilterProp="label"
-                                        options={dataDD}
+                                        <Select
+                                            showSearch placeholder="Add an Agent"
+                                            showArrow mode="multiple"
+                                            onChange={handleChangeAddAgent}
+                                            style={{ width: '100%', padding: '0 5px', lineHeight: '2.4' }}
+                                            optionFilterProp="label"
+                                            options={dataDD}
                                         />
                                     </Col>
                                 </Row>
@@ -199,10 +189,6 @@ function GroupsAgentsCreate({ initProps, dataProfile, dataListAccount, sidemenu 
                         </div>
                     </Form>
                     <div className={`${st.grupdesc} flex flex-col space-y-3 px-4`}>
-                        {/* <div className="font-semibold text-base">Groups</div>
-                        <p className="font-normal text-base">
-                            You can organize your agents into specific Groups like “Sales” and “Product Management”. Segmenting them into divisions lets you easily assign tickets, create specific canned responses, manage workflows and generate group-level reports. Note that the same agent can be a member of multiple groups as well
-                        </p> */}
                     </div>
                 </div>
             </>
@@ -238,7 +224,7 @@ export async function getServerSideProps({ req, res }) {
     const resjsonGP = await resourcesGP.json()
     const dataProfile = resjsonGP
 
-    const resourcesLA = await fetch(`https://boiling-thicket-46501.herokuapp.com/getAccountList`, {
+    const resourcesLA = await fetch(`https://boiling-thicket-46501.herokuapp.com/getAgentList`, {
         method: `POST`,
         headers: {
             'Authorization': JSON.parse(initProps),
