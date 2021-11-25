0b9e9c2a6e5b65aa4dc090ea251a058977bf9658
nauvaladiyasa <94446028+nauvaladiyasa@users.noreply.github.com>
2021-11-25 16:32:31 +0700

    fix various bugs on landingpage, joinourteam, aboutus'

--- pages/migwebsite/aboutus/index.js
+++ pages/migwebsite/aboutus/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Layout from '../../../components/migwebsite/layout.js'
 import ArrowRightOutlined from '@ant-design/icons/ArrowRightOutlined'
-import {Button} from 'antd'
+import {Card,Button} from 'antd'
 
 function AboutUs({ }) {
 
@@ -11,7 +11,7 @@ function AboutUs({ }) {
             
             <section className={'section2people px-4 md:px-20 lg:px-28 xl:px-40 py-8 md:py-32 text-center justify-center '}>
                 <div className={'pb-8'}>
-                    <p className={'text-2xl md:text-3xl font-bold'}>Operate your business, more efficient and more agile</p>
+                    <p className={'text-2xl md:text-3xl gilroy-bold'}>Operate your business, more efficient and more agile</p>
                 </div>
                 <div className={'pb-12'}>
                     <p className={'text-base w-full font-bold'}>Mitramas Infosys global is supporting you staff augmentation &  delivering software and hardware managed services. We have served multiple industries, bringing the best solutions to financial service companies, start ups, as well as government agencies.</p>
@@ -23,7 +23,7 @@ function AboutUs({ }) {
                     {/* <div className={'block md:hidden flex-col py-4 md:py-4 m-auto'}>
                         <img src="/image-aboutus.png" className={'block'} style={{width:'400px',height:'auto',margin:'0 auto'}}></img>
                     </div> */}
-                    <p className={'pb-8 font-bold'}>
+                    <p className={'pb-8 font-bold gilroy-regular'}>
                     Founded in 2003, Mitramas Infosys Global (MIG)  directly partner with global  providers and collaborate with local IT talents to offer you a seamless technology experience. Experienced  across archipelago for more than decade we have a plethora of experience in the business. With integrity as our core principle, we collaborate with our clients to increase their efficiency and reach long-term business goals.
                     </p>
                     
@@ -40,22 +40,25 @@ function AboutUs({ }) {
                     <p className={'text-base w-full md:w-2/5 font-bold'}>One stop  seamless technology solution to help you achieve business goals and optimize your cost </p>
                 </div>
                 <div className={'grid md:flex my-4 justify-center md:justify-between'}>
-                    <div className={'flex-col flex bg-white mr-0 md:mr-10 p-4 w-full md:w-96'}>
-                        <div className={'min-h-full relative pb-8'} style={{}}>
-                            <img style={{height:'50px', width:'auto'}} src="/image/navbar/hardware.png"></img>
-                            <p className={'text-left py-3 font-bold '}>
-                                Hardware
-                            </p>
-                            <p>
-                                Optimize your cost by leasing and maintenances variety of electronic equipments   
-                            </p>
-                            <div className={'absolute bottom-0'}>
-                                <Link href={{pathname: '/hardware'}}><button className={'pt-4 font-bold text-green-800'}>
-                                    Get yours&nbsp; <ArrowRightOutlined className={'relative'} style={{top:'-2.5px'}}/>
-                                </button></Link> 
+                    <Card hoverable>
+                        <div className={'flex-col flex bg-white mr-0 md:mr-10 p-4 w-full md:w-96'}>
+                            <div className={'min-h-full relative pb-8'} style={{}}>
+                                <img style={{height:'50px', width:'auto'}} src="/image/navbar/hardware.png"></img>
+                                <p className={'text-left py-3 font-bold '}>
+                                    Hardware
+                                </p>
+                                <p>
+                                    Optimize your cost by leasing and maintenances variety of electronic equipments   
+                                </p>
+                                <div className={'absolute bottom-0'}>
+                                    <Link href={{pathname: '/hardware'}}><button className={'pt-4 font-bold text-green-800'}>
+                                        Get yours&nbsp; <ArrowRightOutlined className={'relative'} style={{top:'-2.5px'}}/>
+                                    </button></Link> 
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </Card>
+                    <Card hoverable>
                     <div className={'flex-col bg-white my-5 md:my-0 mx-0 md:mx-10 p-4 w-full md:w-96'}>
                         <div className={'min-h-full relative pb-8'} style={{}}>
                             <img style={{height:'50px', width:'auto', position:'relative', left:'-25px'}} src="/image/navbar/software.png"></img>
@@ -71,10 +74,12 @@ function AboutUs({ }) {
                             </div>
                         </div>
                     </div>
+                    </Card>
+                    <Card hoverable>
                     <div className={'flex-col bg-white mdl-0 md:ml-10 p-4 w-full md:w-96'}>
                         <div className={'min-h-full relative pb-8'} style={{}}>
                             <img style={{height:'50px', width:'auto', position:'relative', left:'-10px'}} src="/image/navbar/talents.png"></img>
-                            <p className={'text-left py-3 font-bold '}>People
+                            <p className={'text-left py-3 font-bold '}>Talents
                             </p>
                             <p>
                                 We help you reduce complexity in talent sourcing and management
@@ -86,6 +91,7 @@ function AboutUs({ }) {
                             </div>
                         </div>
                     </div>
+                    </Card>
                 </div>
             </section>
             
