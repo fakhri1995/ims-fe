12a9b83f5d9f53945dace3a0c518a92fed27c213
kennfatt-mig <kennan@mitrasolusi.group>
2022-02-03 15:35:14 +0700

    Chore: add `@next/bundle-analyzer` to final build file size.
    - It only trigger the analyzer when we add `ANALYZE=true` in CLI. Or instead, use `yarn build:analyze`

--- next.config.js
+++ next.config.js
@@ -1,17 +1,21 @@
-// next.config.js
-module.exports = {
-  async rewrites() {
-    return [
-      { source: '/advantages', destination: '/migwebsite/advantages' },
-      { source: '/hardware', destination: '/migwebsite/hardware' },
-      { source: '/software', destination: '/migwebsite/software' },
-      { source: '/talents', destination: '/migwebsite/talents' },
-      { source: '/aboutus', destination: '/migwebsite/aboutus' },
-      { source: '/joinourteam', destination: '/migwebsite/joinourteam' }, 
-      { source: '/sitemap', destination: '/migwebsite/sitemap' }, 
-      { source: '/term', destination: '/migwebsite/termofuse' }, 
-      { source: '/privacy', destination: '/migwebsite/privacy' }, 
-      { source: '/contactus', destination: '/migwebsite/contactus' }, 
-    ]
-  }
-}
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+/** @type {import("next").NextConfig } */
+const nextConfig = {
+  rewrites: async () => [
+    { source: "/advantages", destination: "/migwebsite/advantages" },
+    { source: "/hardware", destination: "/migwebsite/hardware" },
+    { source: "/software", destination: "/migwebsite/software" },
+    { source: "/talents", destination: "/migwebsite/talents" },
+    { source: "/aboutus", destination: "/migwebsite/aboutus" },
+    { source: "/joinourteam", destination: "/migwebsite/joinourteam" },
+    { source: "/sitemap", destination: "/migwebsite/sitemap" },
+    { source: "/term", destination: "/migwebsite/termofuse" },
+    { source: "/privacy", destination: "/migwebsite/privacy" },
+    { source: "/contactus", destination: "/migwebsite/contactus" },
+  ],
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
