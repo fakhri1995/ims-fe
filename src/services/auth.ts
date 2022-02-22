15acc5133a9cf5c8b01f8ab6089189ea9ffb3d32
kennfatt-mig <kennan@mitrasolusi.group>
2022-02-22 16:25:09 +0700

    feat(`services/auth`): new service to handle all authorization (Login Management) things.

---
+++ src/services/auth.ts
@@ -0,0 +1,8 @@
+import type { AxiosInstance } from "axios";
+import { IGetDetailProfile } from "types/api/login/get-detailprofile";
+
+export class LoginService {
+  static async me(axiosClient: AxiosInstance) {
+    return await axiosClient.get<IGetDetailProfile>("/detailProfile");
+  }
+}
