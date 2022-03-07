5806ff00126660c65f6f2daeafdbc5699393fa2a
kennfatt-mig <kennan@mitrasolusi.group>
2022-03-07 16:56:27 +0700

    feat(`attendance-staff`): implement card for tables in Staff Attendance page.
    - TODO: implement table.

--- src/pages/attendance/staff.tsx
+++ src/pages/attendance/staff.tsx
@@ -1,4 +1,5 @@
-import { DownloadOutlined } from "@ant-design/icons";
+import { AppstoreAddOutlined, DownloadOutlined } from "@ant-design/icons";
+import { Tabs } from "antd";
 import { GetServerSideProps, NextPage } from "next";
 
 import ButtonSys from "components/button";
@@ -16,6 +17,8 @@ import { getAxiosClient } from "lib/axios-client";
 import { AuthService } from "apis/auth";
 
 import { PageBreadcrumbValue, ProtectedPageProps } from "types/common";
+
+const { TabPane } = Tabs;
 
 const StaffAttendancePage: NextPage<ProtectedPageProps> = ({
   dataProfile,
@@ -50,10 +53,40 @@ const StaffAttendancePage: NextPage<ProtectedPageProps> = ({
         </div>
 
         {/* Column 2: Aktivitas card (Table and Tabs), Kehadiran card (table) */}
-        <div className="w-full lg:w-3/5 xl:w-4/5 bg-red-400 space-y-6">
+        <div className="w-full lg:w-3/5 xl:w-4/5 space-y-6">
+          {/* <div className="w-full lg:w-3/5 xl:w-4/5 bg-red-400 space-y-6"> */}
           {/* Card: Aktivitas Table */}
-          <div className="bg-white rounded-md shadow-md p-6">
+          <div className="bg-white rounded-md shadow-md p-6 space-y-6">
             <h3 className="mig-heading--4">Aktivitas</h3>
+
+            <div className="flex items-center justify-between">
+              <Tabs defaultActiveKey="1" className="w-1/2">
+                <TabPane tab="Hari Ini" key="1" />
+                <TabPane tab="Riwayat" key="2" />
+              </Tabs>
+
+              <div className="flex space-x-6 w-1/2 justify-end items-center">
+                <ButtonSys
+                  type="default"
+                  onClick={() => {
+                    alert("Button Unduh Tabel clicked");
+                  }}
+                >
+                  <DownloadOutlined className="mr-2" />
+                  Unduh Tabel
+                </ButtonSys>
+
+                <ButtonSys
+                  type="primary"
+                  onClick={() => {
+                    alert("Button Masukkan Aktivitas Clicked");
+                  }}
+                >
+                  <AppstoreAddOutlined className="mr-2" />
+                  Masukkan Aktivitas
+                </ButtonSys>
+              </div>
+            </div>
           </div>
 
           {/* Card: Kehadiran Table */}
