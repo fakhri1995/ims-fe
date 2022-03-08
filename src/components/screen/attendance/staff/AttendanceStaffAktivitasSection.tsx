46451080650c1839dc695d36083fa9f5117e9c99
kennfatt-mig <kennan@mitrasolusi.group>
2022-03-08 14:07:26 +0700

    feat(`feature/attendance-staff`): implement basic table for Aktivitas section.

--- src/components/screen/attendance/staff/AttendanceStaffAktivitasSection.tsx
+++ src/components/screen/attendance/staff/AttendanceStaffAktivitasSection.tsx
@@ -1,8 +1,13 @@
 import { AppstoreAddOutlined, DownloadOutlined } from "@ant-design/icons";
-import { Tabs } from "antd";
-import type { FC } from "react";
+import { ConfigProvider, Tabs } from "antd";
+import { Table } from "antd";
+import { ColumnsType } from "antd/lib/table";
+import { FC, useCallback, useMemo, useState } from "react";
 
 import ButtonSys from "components/button";
+import { DataEmptyState } from "components/states/DataEmptyState";
+
+import { getAntdTablePaginationConfig } from "lib/standard-config";
 
 const { TabPane } = Tabs;
 
@@ -17,22 +22,56 @@ export interface IAttendanceStaffAktivitasSection {}
 export const AttendanceStaffAktivitasSection: FC<
   IAttendanceStaffAktivitasSection
 > = (props) => {
+  /** 1 => Hari Ini, 2 => Riwayat */
+  const [tabActiveKey, setTabActiveKey] = useState<"1" | "2" | string>("1");
+
+  /**
+   * TODO: generate tableColumns dynamically sesuai data backend
+   */
+  const tableColums = useMemo<ColumnsType<{}>>(
+    () => {
+      return [
+        {
+          key: "id",
+          title: "No.",
+          render: (_, __, index) => `${++index}`,
+          width: 64,
+        },
+        {
+          key: "id",
+          title: "Waktu Pengisian",
+          sorter: true,
+        },
+      ];
+    },
+    [
+      /** TODO */
+    ]
+  );
+
+  const tablePaginationConf = useMemo(
+    () => getAntdTablePaginationConfig(),
+    [
+      /**TODO */
+    ]
+  );
+
+  const onRowItemClicked = useCallback((datum) => {
+    alert(`Row with id ${datum} is clicked!`);
+  }, []);
+
   return (
     <section className="mig-platform space-y-6">
       <h3 className="mig-heading--4">Aktivitas</h3>
 
       <div className="flex items-center justify-between">
-        <Tabs
-          defaultActiveKey="1"
-          className="w-1/2"
-          onChange={(activeKey) => console.log(activeKey)}
-        >
+        <Tabs defaultActiveKey="1" className="w-1/2" onChange={setTabActiveKey}>
           <TabPane tab="Hari Ini" key="1" />
           <TabPane tab="Riwayat" key="2" />
         </Tabs>
 
         <div className="flex space-x-6 w-1/2 justify-end items-center">
-          <ButtonSys
+          {/* <ButtonSys
             type="default"
             onClick={() => {
               alert("Button Unduh Tabel clicked");
@@ -40,7 +79,7 @@ export const AttendanceStaffAktivitasSection: FC<
           >
             <DownloadOutlined className="mr-2" />
             Unduh Tabel
-          </ButtonSys>
+          </ButtonSys> */}
 
           <ButtonSys
             type="primary"
@@ -53,6 +92,25 @@ export const AttendanceStaffAktivitasSection: FC<
           </ButtonSys>
         </div>
       </div>
+
+      <ConfigProvider
+        renderEmpty={() => (
+          <DataEmptyState caption="Belum ada aktivitas. Silakan masukkan aktivitas untuk hari ini" />
+        )}
+      >
+        <Table
+          columns={tableColums}
+          // TODO: render data berdasarkan state si tab
+          dataSource={[]}
+          pagination={tablePaginationConf}
+          onRow={(datum) => {
+            return {
+              className: "hover:cursor-pointer",
+              onClick: () => onRowItemClicked(datum),
+            };
+          }}
+        />
+      </ConfigProvider>
     </section>
   );
 };
