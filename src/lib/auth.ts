797f8547ad61bf112c2ad296f1d87717501d4935
kennfatt-mig <kennan@mitrasolusi.group>
2022-04-07 11:36:35 +0700

    fix: landingpage warning karena penggunaan attribute `class` instead of `className`.
    - remove `lib/axios-client` karena saat ini tidak dipakai.
    - remove `lib/auth` parseToken function karena saat ini tidak dipakai.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,47 +1,6 @@
 import Cookies from "js-cookie";
-import { GetServerSidePropsContext } from "next";
-
-import httpcookie from "cookie";
 
 export const TOKEN_COOKIE_NAME = "token";
-
-/**
- * Server side function.
- *
- * A function to reads the cookie from incoming HTTP request headers and extract
- *  specifiec TOKEN_COOKIE_NAME from it.
- *
- * It does not validating the cookie to the backend. It just read and extract.
- *
- * @example
- * ```ts
- * const { token, hasNoToken } = parseToken(ctx);
- * if (hasNoToken) {
- *   // when there is no "token" in incoming HTTP request's cookie
- * }
- *
- * // token is exists but it's not always a valid token.
- * token;
- * ```
- */
-export const parseToken = ({ req }: GetServerSidePropsContext) => {
-  const { cookie } = req.headers;
-
-  let hasNoToken = true;
-  let token = "";
-
-  if (cookie) {
-    let cookies = httpcookie.parse(cookie);
-    if (TOKEN_COOKIE_NAME in cookies) {
-      hasNoToken = false;
-
-      /** The token is being stringified on login though... */
-      token = JSON.parse(cookies[TOKEN_COOKIE_NAME]);
-    }
-  }
-
-  return { token, hasNoToken };
-};
 
 /**
  * Retrieve token string from cookie.
