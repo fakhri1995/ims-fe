7be82ee455db3ca790df945bf527eae91de17594
yasminadeliaMIG <yasminadelia@yahoo.co.id>
2022-09-08 22:01:50 +0700

    feat: add resume icon and assessment table

--- src/components/icon.js
+++ src/components/icon.js
@@ -1827,6 +1827,31 @@ const Iconquestion = (props) => {
   return <Icon component={svgquestion} style={{ marginRight: `0.5rem` }} />;
 };
 
+const ResumeIconSvg = () => {
+  const resumeIconSvg = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-file-text"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="#597e8d"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+      <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" />
+      <line x1="9" y1="9" x2="10" y2="9" />
+      <line x1="9" y1="13" x2="15" y2="13" />
+      <line x1="9" y1="17" x2="15" y2="17" />
+    </svg>
+  );
+  return <Icon component={resumeIconSvg}></Icon>;
+};
+
 export {
   DashboardIconSvg,
   TicketIconSvg,
@@ -1901,4 +1926,5 @@ export {
   PlusIconSvg,
   XIconSvg,
   ArmChairlIconSvg,
+  ResumeIconSvg,
 };
--- src/components/table/tableCustom.js
+++ src/components/table/tableCustom.js
@@ -1058,6 +1058,112 @@ const TableCustomTickets = ({
   );
 };
 
+const TableCustomRoleAssessment = ({
+  dataSource,
+  setDataSource,
+  columns,
+  loading,
+  pageSize,
+  total,
+  setpraloading,
+  initProps,
+  setpage,
+  pagefromsearch,
+  setdataraw,
+  setsorting,
+  sorting,
+  searching,
+  criteriaCount,
+  candidateCount,
+  dataprofile,
+}) => {
+  const rt = useRouter();
+  const [rowstate, setrowstate] = useState(0);
+  return (
+    <Table
+      className="tableTypeTask"
+      dataSource={dataSource}
+      columns={columns}
+      loading={loading}
+      scroll={{ x: 200 }}
+      pagination={{
+        current: pagefromsearch,
+        pageSize: pageSize,
+        total: total,
+        onChange: (page, pageSize) => {
+          setpraloading(true);
+          setpage(page);
+          fetch(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/getAssessments?sort_by=${sorting.sort_by}&sort_type=${sorting.sort_type}&keyword=${searching}&page=${page}&rows=${pageSize}`,
+            {
+              method: `GET`,
+              headers: {
+                Authorization: JSON.parse(initProps),
+              },
+            }
+          )
+            .then((res) => res.json())
+            .then((res2) => {
+              setdataraw(res2.data);
+              setDataSource(res2.data.data);
+              setpraloading(false);
+            });
+        },
+      }}
+      onChange={(pagination, filters, sorter, extra) => {
+        if (extra.action === "sort") {
+          if (sorter.column) {
+            setpraloading(true);
+            setsorting({
+              sort_by: sorter.column.dataIndex,
+              sort_type: sorter.order === "ascend" ? "asc" : "desc",
+            });
+            fetch(
+              `${process.env.NEXT_PUBLIC_BACKEND_URL}/getAssessments?sort_by=${
+                sorter.column.dataIndex
+              }&sort_type=${
+                sorter.order === "ascend" ? "asc" : "desc"
+              }&keyword=${searching}&page=${pagination.current}&rows=${
+                pagination.pageSize
+              }`,
+              {
+                method: `GET`,
+                headers: {
+                  Authorization: JSON.parse(initProps),
+                },
+              }
+            )
+              .then((res) => res.json())
+              .then((res2) => {
+                setdataraw(res2.data);
+                setDataSource(res2.data.data);
+                setpraloading(false);
+              });
+          } else {
+            setpraloading(true);
+            setsorting({ sort_by: "", sort_type: "" });
+            fetch(
+              `${process.env.NEXT_PUBLIC_BACKEND_URL}/getAssessments?sort_by=&sort_type=&keyword=${searching}&page=${pagination.current}&rows=${pagination.pageSize}`,
+              {
+                method: `GET`,
+                headers: {
+                  Authorization: JSON.parse(initProps),
+                },
+              }
+            )
+              .then((res) => res.json())
+              .then((res2) => {
+                setdataraw(res2.data);
+                setDataSource(res2.data.data);
+                setpraloading(false);
+              });
+          }
+        }
+      }}
+    />
+  );
+};
+
 export {
   TableCustom,
   TableCustomRelasi,
@@ -1069,4 +1175,5 @@ export {
   TableCustomTicketTypes,
   TableCustomTicketHistories,
   TableCustomTickets,
+  TableCustomRoleAssessment,
 };
