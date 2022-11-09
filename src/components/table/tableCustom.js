3cb44aa50d3a32bbf60a3cb408ae6f4bde38022f
yasminadeliaMIG <yasminadelia@mitrasolusi.group>
2022-11-09 09:07:39 +0700

    feat: implement "Daftar Karyawan" page

--- src/components/table/tableCustom.js
+++ src/components/table/tableCustom.js
@@ -1338,11 +1338,6 @@ const TableCustomRecruitmentCandidate = ({
       //   selectedRows
       // );
     },
-    // getCheckboxProps: (record) => ({
-    //   disabled: record.name === 'Disabled User',
-    //   // Column configuration not to be checked
-    //   name: record.name,
-    // }),
   };
   return (
     <Table
@@ -2074,6 +2069,138 @@ const TableCustomRecruitmentTemplateEmail = ({
   );
 };
 
+const TableCustomEmployeeList = ({
+  dataSource,
+  setDataSource,
+  columns,
+  loading,
+  pageSize,
+  total,
+  setpraloading,
+  initProps,
+  setpage,
+  pagefromsearch,
+  setdataraw,
+  setsorting,
+  sorting,
+  searching,
+  selectedRoleId,
+  selectedContractStatus,
+  tempIdClicked,
+  setTriggerRowClicked,
+}) => {
+  const rt = useRouter();
+  const [rowstate, setrowstate] = useState(0);
+  return (
+    <Table
+      dataSource={dataSource}
+      columns={columns}
+      rowKey={(record) => record.id}
+      loading={loading}
+      scroll={{ x: 200 }}
+      pagination={{
+        current: pagefromsearch,
+        pageSize: pageSize,
+        total: total,
+        onChange: (page, pageSize) => {
+          setpraloading(true);
+          setpage(page);
+          fetch(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/getRecruitments?recruitment_role_id=${selectedRoleId}&recruitment_status_id=${selectedContractStatus}&sort_by=${sorting.sort_by}&sort_type=${sorting.sort_type}&keyword=${searching}&page=${page}&rows=${pageSize}`,
+            {
+              method: `GET`,
+              headers: {
+                Authorization: JSON.parse(initProps),
+              },
+            }
+          )
+            .then((res) => res.json())
+            .then((res2) => {
+              setdataraw(res2.data);
+              setDataSource(res2.data.data);
+              setpraloading(false);
+            });
+        },
+      }}
+      onChange={(pagination, filters, sorter, extra) => {
+        if (extra.action === "sort") {
+          if (sorter.column) {
+            setpraloading(true);
+            setsorting({
+              sort_by: sorter.column.dataIndex,
+              sort_type: sorter.order === "ascend" ? "asc" : "desc",
+            });
+            fetch(
+              `${
+                process.env.NEXT_PUBLIC_BACKEND_URL
+              }/getRecruitments?recruitment_role_id=${selectedRoleId}&recruitment_status_id=${selectedContractStatus}&sort_by=${
+                sorter.column.dataIndex
+              }&sort_type=${
+                sorter.order === "ascend" ? "asc" : "desc"
+              }&keyword=${searching}&page=${pagination.current}&rows=${
+                pagination.pageSize
+              }`,
+              {
+                method: `GET`,
+                headers: {
+                  Authorization: JSON.parse(initProps),
+                },
+              }
+            )
+              .then((res) => res.json())
+              .then((res2) => {
+                setdataraw(res2.data);
+                setDataSource(res2.data.data);
+                setpraloading(false);
+              })
+              .catch((err) => {
+                // console.log(err);
+                notification.error({
+                  message: `${err.message}`,
+                  duration: 3,
+                });
+                setpraloading(false);
+              });
+          } else {
+            setpraloading(true);
+            setsorting({ sort_by: "", sort_type: "" });
+            fetch(
+              `${process.env.NEXT_PUBLIC_BACKEND_URL}/getRecruitments?recruitment_role_id=${selectedRoleId}&recruitment_status_id=${selectedContractStatus}&sort_by=&sort_type=&keyword=${searching}&page=${pagination.current}&rows=${pagination.pageSize}`,
+              {
+                method: `GET`,
+                headers: {
+                  Authorization: JSON.parse(initProps),
+                },
+              }
+            )
+              .then((res) => res.json())
+              .then((res2) => {
+                setdataraw(res2.data);
+                setDataSource(res2.data.data);
+                setpraloading(false);
+              });
+          }
+        }
+      }}
+      onRow={(record, rowIndex) => {
+        return {
+          onMouseOver: () => {
+            setrowstate(record.id);
+          },
+          onClick: () => {
+            tempIdClicked.current = record.id;
+            setTriggerRowClicked((prev) => prev + 1);
+          },
+        };
+      }}
+      rowClassName={(record, idx) => {
+        return `${record.id === rowstate && `cursor-pointer`}
+        }`;
+      }}
+    />
+  );
+};
+
 export {
   TableCustom,
   TableCustomRelasi,
@@ -2093,4 +2220,5 @@ export {
   TableCustomRecruitmentStatus,
   TableCustomRecruitmentRegistration,
   TableCustomRecruitmentTemplateEmail,
+  TableCustomEmployeeList,
 };
