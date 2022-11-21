5436d02820e15d178f661a0281364da5be79b992
yasminadeliaMIG <yasminadelia@mitrasolusi.group>
2022-11-21 17:39:33 +0700

    feat: add "Karyawan" and "Profil" menu

--- src/components/icon.js
+++ src/components/icon.js
@@ -2234,6 +2234,28 @@ const CircleCheckIconSvg = ({ size, color }) => {
   return <Icon component={circleCheckIconSvg}></Icon>;
 };
 
+const LogoutIconSvg = ({ size, color }) => {
+  const logoutIconSvg = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="icon icon-tabler icon-tabler-logout"
+      width={size ? size : 20}
+      height={size ? size : 20}
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke={color ? color : "#4D4D4D"}
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+      <path d="M7 12h14l-3 -3m0 6l3 -3" />
+    </svg>
+  );
+  return <Icon component={logoutIconSvg}></Icon>;
+};
+
 export {
   DashboardIconSvg,
   TicketIconSvg,
@@ -2326,4 +2348,5 @@ export {
   RightIconSvg,
   LeftIconSvg,
   CircleCheckIconSvg,
+  LogoutIconSvg,
 };
