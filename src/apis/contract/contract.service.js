ecd79c7b1d593f01e99b14087dce435a512e77a1
yasminadeliaMIG <yasminadelia@mitrasolusi.group>
2023-08-15 23:20:32 +0700

    feat: consume API in invoice table

--- src/apis/contract/contract.service.js
+++ src/apis/contract/contract.service.js
@@ -228,4 +228,44 @@ export class ContractService {
 
     return apiRes;
   };
+
+  static getInvoices = async (initProps, feature, queryParams, keyword) => {
+    if (!feature) {
+      permissionWarningNotification("Mendapatkan", "Daftar Invoice Kontrak");
+      return;
+    }
+
+    const payload = QueryString.stringify(queryParams, {
+      addQueryPrefix: true,
+    });
+
+    const apiRes = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/getContractInvoices${payload}&keyword=${keyword}`,
+      {
+        method: `GET`,
+        headers: {
+          Authorization: JSON.parse(initProps),
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((res2) => {
+        if (res2.success) {
+          return res2;
+        } else {
+          notification.error({
+            message: `${res2.message}`,
+            duration: 3,
+          });
+        }
+      })
+      .catch((err) => {
+        notification.error({
+          message: `${err.response}`,
+          duration: 3,
+        });
+      });
+
+    return apiRes;
+  };
 }
