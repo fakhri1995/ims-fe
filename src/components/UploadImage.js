f3fb6de43e9f77ab9515221d4567ca4eda566c31
yasminadeliaMIG <yasminadelia@mitrasolusi.group>
2023-12-05 16:37:28 +0700

    refactor `resume`: upload image component

---
+++ src/components/UploadImage.js
@@ -0,0 +1,283 @@
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, Spin, Upload } from "antd";
+import React from "react";
+import { useRef } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useCallback } from "react";
+import ReactCrop, { centerCrop, makeAspectCrop } from "react-image-crop";
+
+import canvasPreview from "lib/canvasPreview";
+
+import { beforeUploadFileMaxSize } from "../lib/helper";
+import ButtonSys from "./button";
+import { CheckIconSvg } from "./icon";
+import ModalCore from "./modal/modalCore";
+
+const UploadImage = ({ useCrop = false, dataDisplay, setDataUpdate }) => {
+  const [fileList, setFileList] = useState([]);
+  const [fileName, setFileName] = useState("");
+  const [uploadPictureLoading, setUploadPictureLoading] = useState(false);
+
+  // Modal crop state
+  const [modalCrop, setModalCrop] = useState(false);
+  const [imgSrc, setImgSrc] = useState("");
+  const previewCanvasRef = useRef(null);
+  const imgRef = useRef(null);
+  const [crop, setCrop] = useState({
+    unit: "px", // Can be 'px' or '%'
+    x: 25,
+    y: 25,
+    width: 100,
+    height: 115,
+  });
+
+  const [completedCrop, setCompletedCrop] = useState(null);
+  const [aspect, setAspect] = useState(100 / 115);
+
+  // Display profile photo filename when available
+  useEffect(() => {
+    if (dataDisplay?.profile_image?.link) {
+      const currentFileName = dataDisplay?.profile_image?.link?.split("/")[2];
+      setFileList([{ name: currentFileName }]);
+    } else {
+      setFileList([]);
+    }
+  }, [dataDisplay?.profile_image]);
+
+  useEffect(() => {
+    const preview = async () => {
+      if (
+        completedCrop?.width &&
+        completedCrop?.height &&
+        imgRef.current &&
+        previewCanvasRef.current
+      ) {
+        // We use canvasPreview as it's much faster than imgPreview.
+        canvasPreview(imgRef.current, previewCanvasRef.current, completedCrop);
+      }
+    };
+    const timer = setTimeout(() => preview(), 100);
+    return () => clearTimeout(timer);
+  }, [completedCrop]);
+
+  // Handle upload file
+  const beforeUploadImage = useCallback((uploadedFile) => {
+    const checkMaxFileSizeFilter = beforeUploadFileMaxSize(2);
+    const isReachedMaxFileSize =
+      checkMaxFileSizeFilter(uploadedFile) === Upload.LIST_IGNORE;
+    const allowedFileTypes = [`image/png`, `image/jpg`, `image/jpeg`];
+
+    if (!allowedFileTypes.includes(uploadedFile.type)) {
+      notification.error({
+        message: "File harus berupa gambar",
+      });
+      return Upload.LIST_IGNORE;
+    }
+
+    if (isReachedMaxFileSize) {
+      return Upload.LIST_IGNORE;
+    }
+
+    useCrop && setModalCrop(true);
+  }, []);
+
+  const onUploadChange = useCallback(({ file }) => {
+    if (useCrop) {
+      if (file?.originFileObj) {
+        setCrop(undefined); // Makes crop preview update between images.
+        const reader = new FileReader();
+        reader.addEventListener("load", () =>
+          setImgSrc(reader.result?.toString() || "")
+        );
+        reader.readAsDataURL(file.originFileObj);
+        setFileName(file?.name);
+      }
+    } else {
+      setUploadPictureLoading(file.status === "uploading");
+      if (file.status !== "removed") {
+        setFileList([file]);
+        setDataUpdate((prev) => ({
+          ...prev,
+          profile_image: file?.originFileObj,
+        }));
+      }
+    }
+  }, []);
+
+  const onUploadSubmit = async () => {
+    setUploadPictureLoading(true);
+    const image = imgRef.current;
+
+    const previewCanvas = previewCanvasRef.current;
+    if (!image || !previewCanvas || !completedCrop) {
+      notification.error({
+        message: "Crop canvas does not exist",
+      });
+      throw new Error("Crop canvas does not exist");
+    }
+
+    // This will size relative to the uploaded image
+    // size. If you want to size according to what they
+    // are looking at on screen, remove scaleX + scaleY
+    // const scaleX = image.naturalWidth / image.width;
+    // const scaleY = image.naturalHeight / image.height;
+
+    const offscreen = new OffscreenCanvas(
+      completedCrop.width,
+      completedCrop.height
+    );
+    const ctx = offscreen.getContext("2d");
+    if (!ctx) {
+      notification.error({
+        message: "No 2d context",
+      });
+      throw new Error("No 2d context");
+    }
+
+    ctx.drawImage(
+      previewCanvas,
+      0,
+      0,
+      previewCanvas.width,
+      previewCanvas.height,
+      0,
+      0,
+      offscreen.width,
+      offscreen.height
+    );
+
+    // You might want { type: "image/jpeg", quality: <0 to 1> } to
+    // reduce image size
+    const blob = await offscreen.convertToBlob({
+      type: "image/png",
+      quality: 0.7,
+    });
+
+    // Create a File object from the Blob
+    const imageFile = new File([blob], fileName, {
+      type: "image/jpeg",
+    });
+
+    setFileList([imageFile]);
+    setDataUpdate((prev) => ({
+      ...prev,
+      profile_image: imageFile,
+    }));
+
+    setModalCrop(false);
+    setUploadPictureLoading(false);
+  };
+
+  const onUploadRemove = useCallback(() => {
+    setFileList([]);
+    setDataUpdate((prev) => ({
+      ...prev,
+      profile_image: null,
+    }));
+  }, []);
+
+  const onImageLoad = (e) => {
+    if (aspect) {
+      const { width, height } = e.currentTarget;
+      setCrop(centerAspectCrop(width, height, aspect));
+    }
+  };
+
+  const centerAspectCrop = (mediaWidth, mediaHeight, aspect) => {
+    return centerCrop(
+      makeAspectCrop(
+        {
+          unit: "px",
+          width: 100,
+          height: 115,
+        },
+        aspect,
+        mediaWidth,
+        mediaHeight
+      ),
+      mediaWidth,
+      mediaHeight
+    );
+  };
+
+  return (
+    <>
+      <Upload
+        accept=".png, .jpg, .jpeg"
+        listType="picture"
+        maxCount={1}
+        beforeUpload={beforeUploadImage}
+        onChange={onUploadChange}
+        onRemove={onUploadRemove}
+        disabled={uploadPictureLoading}
+        fileList={fileList}
+      >
+        <Button
+          className="btn-sm btn font-semibold px-6 space-x-2 border
+                  text-primary100 hover:text-white bg-white border-primary100 
+                  hover:bg-primary75 hover:border-primary75  
+                  focus:border-primary75 focus:text-primary100 "
+        >
+          <UploadOutlined />
+          <p>Unggah File</p>
+        </Button>
+      </Upload>
+
+      {useCrop && (
+        <ModalCore
+          title={"Edit Photo"}
+          visible={modalCrop}
+          onCancel={() => setModalCrop(false)}
+          footer={
+            <Spin spinning={uploadPictureLoading}>
+              <ButtonSys
+                type={"primary"}
+                onClick={onUploadSubmit}
+                disabled={!completedCrop}
+              >
+                <div className="flex flex-row space-x-2 items-center">
+                  <CheckIconSvg color={"white"} size={16} />
+                  <p>Simpan</p>
+                </div>
+              </ButtonSys>
+            </Spin>
+          }
+        >
+          <>
+            <ReactCrop
+              crop={crop}
+              onChange={(_, percentCrop) => setCrop(percentCrop)}
+              onComplete={(c) => setCompletedCrop(c)}
+              aspect={aspect}
+              // minWidth={400}
+              minHeight={100}
+            >
+              <img
+                ref={imgRef}
+                alt="Crop image"
+                src={imgSrc}
+                onLoad={onImageLoad}
+              />
+            </ReactCrop>
+            {!!completedCrop && (
+              <div className="flex justify-end">
+                <canvas
+                  ref={previewCanvasRef}
+                  style={{
+                    border: "1px solid black",
+                    objectFit: "contain",
+                    width: completedCrop.width,
+                    height: completedCrop.height,
+                  }}
+                />
+              </div>
+            )}
+          </>
+        </ModalCore>
+      )}
+    </>
+  );
+};
+
+export default UploadImage;
--- src/components/cards/resume/BasicInfoCard.js
+++ src/components/cards/resume/BasicInfoCard.js
@@ -2,39 +2,19 @@ import {
   CloseOutlined,
   DeleteOutlined,
   DownloadOutlined,
-  UploadOutlined,
 } from "@ant-design/icons";
 import { PDFDownloadLink } from "@react-pdf/renderer";
-import {
-  Button,
-  Form,
-  Input,
-  Modal,
-  Select,
-  Spin,
-  Switch,
-  Upload,
-  notification,
-} from "antd";
-import TextArea from "antd/lib/input/TextArea";
-import moment from "moment";
+import { Form, Input, Select, Spin, Switch } from "antd";
 import { useRouter } from "next/router";
-import React, { useRef } from "react";
+import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
-import { useCallback } from "react";
-import ReactCrop, { centerCrop, makeAspectCrop } from "react-image-crop";
 
 import { AccessControl } from "components/features/AccessControl";
 
-import canvasPreview from "lib/canvasPreview";
 import { RESUME_DELETE, RESUME_GET } from "lib/features";
 
-import { RESUME_UPDATE } from "../../../lib/features";
-import {
-  beforeUploadFileMaxSize,
-  generateStaticAssetUrl,
-} from "../../../lib/helper";
+import { generateStaticAssetUrl } from "../../../lib/helper";
 import UploadImage from "../../UploadImage";
 import ButtonSys from "../../button";
 import {
@@ -72,24 +52,6 @@ const BasicInfoCard = ({
   const [modalDelete, setModalDelete] = useState(false);
   const [openDownloadModal, setOpenDownloadModal] = useState(false);
   const [showLogoStatus, setShowLogoStatus] = useState(true);
-  const [fileList, setFileList] = useState([]);
-  const [fileName, setFileName] = useState("");
-  const [uploadPictureLoading, setUploadPictureLoading] = useState(false);
-
-  // Modal crop state
-  const [modalCrop, setModalCrop] = useState(false);
-  const [imgSrc, setImgSrc] = useState("");
-  const previewCanvasRef = useRef(null);
-  const imgRef = useRef(null);
-  const [crop, setCrop] = useState({
-    unit: "px", // Can be 'px' or '%'
-    x: 25,
-    y: 25,
-    width: 100,
-    height: 115,
-  });
-  const [completedCrop, setCompletedCrop] = useState(null);
-  const [aspect, setAspect] = useState(100 / 115);
 
   const onChangeInput = (e) => {
     setDataUpdateBasic((prev) => ({
@@ -104,161 +66,6 @@ const BasicInfoCard = ({
     setIsOnClient(true);
   }, []);
 
-  // Display profile photo filename when available
-  useEffect(() => {
-    if (dataDisplay?.profile_image?.link) {
-      const currentFileName = dataDisplay?.profile_image?.link?.split("/")[2];
-      setFileList([{ name: currentFileName }]);
-    } else {
-      setFileList([]);
-    }
-  }, [dataDisplay?.profile_image]);
-
-  useEffect(() => {
-    const preview = async () => {
-      if (
-        completedCrop?.width &&
-        completedCrop?.height &&
-        imgRef.current &&
-        previewCanvasRef.current
-      ) {
-        // We use canvasPreview as it's much faster than imgPreview.
-        canvasPreview(imgRef.current, previewCanvasRef.current, completedCrop);
-      }
-    };
-    const timer = setTimeout(() => preview(), 100);
-    return () => clearTimeout(timer);
-  }, [completedCrop]);
-
-  // Handle upload file
-  const beforeUploadImage = useCallback((uploadedFile) => {
-    const checkMaxFileSizeFilter = beforeUploadFileMaxSize(2);
-    const isReachedMaxFileSize =
-      checkMaxFileSizeFilter(uploadedFile) === Upload.LIST_IGNORE;
-    const allowedFileTypes = [`image/png`, `image/jpg`, `image/jpeg`];
-
-    if (!allowedFileTypes.includes(uploadedFile.type)) {
-      notification.error({
-        message: "File harus berupa gambar",
-      });
-      return Upload.LIST_IGNORE;
-    }
-
-    if (isReachedMaxFileSize) {
-      return Upload.LIST_IGNORE;
-    }
-
-    setModalCrop(true);
-  }, []);
-
-  const onUploadChange = useCallback(({ file }) => {
-    if (file?.originFileObj) {
-      setCrop(undefined); // Makes crop preview update between images.
-      const reader = new FileReader();
-      reader.addEventListener("load", () =>
-        setImgSrc(reader.result?.toString() || "")
-      );
-      reader.readAsDataURL(file.originFileObj);
-      setFileName(file?.name);
-    }
-  }, []);
-
-  const onUploadSubmit = async () => {
-    setUploadPictureLoading(true);
-    const image = imgRef.current;
-
-    const previewCanvas = previewCanvasRef.current;
-    if (!image || !previewCanvas || !completedCrop) {
-      notification.error({
-        message: "Crop canvas does not exist",
-      });
-      throw new Error("Crop canvas does not exist");
-    }
-
-    // This will size relative to the uploaded image
-    // size. If you want to size according to what they
-    // are looking at on screen, remove scaleX + scaleY
-    // const scaleX = image.naturalWidth / image.width;
-    // const scaleY = image.naturalHeight / image.height;
-
-    const offscreen = new OffscreenCanvas(
-      completedCrop.width,
-      completedCrop.height
-    );
-    const ctx = offscreen.getContext("2d");
-    if (!ctx) {
-      notification.error({
-        message: "No 2d context",
-      });
-      throw new Error("No 2d context");
-    }
-
-    ctx.drawImage(
-      previewCanvas,
-      0,
-      0,
-      previewCanvas.width,
-      previewCanvas.height,
-      0,
-      0,
-      offscreen.width,
-      offscreen.height
-    );
-
-    // You might want { type: "image/jpeg", quality: <0 to 1> } to
-    // reduce image size
-    const blob = await offscreen.convertToBlob({
-      type: "image/png",
-      quality: 0.7,
-    });
-
-    // Create a File object from the Blob
-    const imageFile = new File([blob], fileName, {
-      type: "image/jpeg",
-    });
-
-    setFileList([imageFile]);
-    setDataUpdateBasic((prev) => ({
-      ...prev,
-      profile_image: imageFile,
-    }));
-
-    setModalCrop(false);
-    setUploadPictureLoading(false);
-  };
-
-  const onUploadRemove = useCallback(() => {
-    setFileList([]);
-    setDataUpdateBasic((prev) => ({
-      ...prev,
-      profile_image: null,
-    }));
-  }, []);
-
-  const onImageLoad = (e) => {
-    if (aspect) {
-      const { width, height } = e.currentTarget;
-      setCrop(centerAspectCrop(width, height, aspect));
-    }
-  };
-
-  const centerAspectCrop = (mediaWidth, mediaHeight, aspect) => {
-    return centerCrop(
-      makeAspectCrop(
-        {
-          unit: "px",
-          width: 100,
-          height: 115,
-        },
-        aspect,
-        mediaWidth,
-        mediaHeight
-      ),
-      mediaWidth,
-      mediaHeight
-    );
-  };
-
   // console.log(dataUpdateBasic);
   // console.log(dataDisplay)
   return (
@@ -269,7 +76,7 @@ const BasicInfoCard = ({
             <h3 className="mig-heading--3">Basic Information</h3>
             <div
               className="flex flex-col md:flex-row space-y-2 md:space-y-0 
-          md:space-x-6 items-end md:items-center"
+              md:space-x-6 items-end md:items-center"
             >
               <ButtonSys
                 type={"default"}
@@ -338,31 +145,11 @@ const BasicInfoCard = ({
               <em className="text-mono50 mr-10">
                 Unggah File JPEG (Maksimal 2 MB)
               </em>
-              {/* <UploadImage
+              <UploadImage
                 useCrop={true}
                 dataDisplay={dataDisplay}
                 setDataUpdate={setDataUpdateBasic}
-              /> */}
-              <Upload
-                accept=".png, .jpg, .jpeg"
-                listType="picture"
-                maxCount={1}
-                beforeUpload={beforeUploadImage}
-                onChange={onUploadChange}
-                onRemove={onUploadRemove}
-                disabled={uploadPictureLoading}
-                fileList={fileList}
-              >
-                <Button
-                  className="btn-sm btn font-semibold px-6 space-x-2 border
-                  text-primary100 hover:text-white bg-white border-primary100 
-                  hover:bg-primary75 hover:border-primary75  
-                  focus:border-primary75 focus:text-primary100 "
-                >
-                  <UploadOutlined />
-                  <p>Unggah File</p>
-                </Button>
-              </Upload>
+              />
             </Form.Item>
             <Form.Item
               label="Name"
@@ -675,59 +462,6 @@ const BasicInfoCard = ({
             dengan nama <strong>{dataDisplay?.name}</strong>?
           </p>
         </ModalHapus2>
-      </AccessControl>
-
-      <AccessControl hasPermission={RESUME_UPDATE}>
-        <ModalCore
-          title={"Edit Photo"}
-          visible={modalCrop}
-          onCancel={() => setModalCrop(false)}
-          footer={
-            <Spin spinning={uploadPictureLoading}>
-              <ButtonSys
-                type={"primary"}
-                onClick={onUploadSubmit}
-                disabled={!completedCrop}
-              >
-                <div className="flex flex-row space-x-2 items-center">
-                  <CheckIconSvg color={"white"} size={16} />
-                  <p>Simpan</p>
-                </div>
-              </ButtonSys>
-            </Spin>
-          }
-        >
-          <>
-            <ReactCrop
-              crop={crop}
-              onChange={(_, percentCrop) => setCrop(percentCrop)}
-              onComplete={(c) => setCompletedCrop(c)}
-              aspect={aspect}
-              // minWidth={400}
-              minHeight={100}
-            >
-              <img
-                ref={imgRef}
-                alt="Crop image"
-                src={imgSrc}
-                onLoad={onImageLoad}
-              />
-            </ReactCrop>
-            {!!completedCrop && (
-              <div className="flex justify-end">
-                <canvas
-                  ref={previewCanvasRef}
-                  style={{
-                    border: "1px solid black",
-                    objectFit: "contain",
-                    width: completedCrop.width,
-                    height: completedCrop.height,
-                  }}
-                />
-              </div>
-            )}
-          </>
-        </ModalCore>
       </AccessControl>
     </>
   );
