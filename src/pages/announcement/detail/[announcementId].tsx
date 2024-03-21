032f7eb71b9edf9fb1eca630be45adab5fbee917
yasminadeliaMIG <yasminadelia@mitrasolusi.group>
2024-03-21 14:16:05 +0700

    feat: change folder name

--- src/pages/announcement/detail/[announcementId].tsx
+++ src/pages/announcement/detail/[announcementId].tsx
@@ -1,21 +1,13 @@
-import { DeleteOutlined } from "@ant-design/icons";
 import { Skeleton, Spin, notification } from "antd";
-import parse from "html-react-parser";
 import { GetServerSideProps, NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 
-import ButtonSys from "components/button";
 import { AccessControl } from "components/features/AccessControl";
-import { ArrowLeftIconSvg } from "components/icon";
 import LayoutDashboard from "components/layout-dashboardNew";
 import { ModalHapus2 } from "components/modal/modalCustom";
-import {
-  AttendanceDetailEvidenceSection,
-  AttendanceDetailFormAttendanceSection,
-  AttendanceDetailMetaCard,
-} from "components/screen/attendance";
+import { AnnouncementMessageSection } from "components/screen/announcement/AnnouncementMessageSection";
 
 import { useAccessControl } from "contexts/access-control";
 
@@ -25,13 +17,7 @@ import {
   ANNOUNCEMENTS_GET,
   ANNOUNCEMENT_DELETE,
   ANNOUNCEMENT_GET,
-  ATTENDANCE_USER_ADMIN_GET,
-  ATTENDANCE_USER_GET,
 } from "lib/features";
-import {
-  generateStaticAssetUrl,
-  permissionWarningNotification,
-} from "lib/helper";
 
 import { AnnouncementService } from "apis/announcement";
 
@@ -131,62 +117,10 @@ const AnnouncementDetailPage: NextPage<ProtectedPageProps> = ({
       sidemenu="announcement"
     >
       <div className="px-5">
-        <Spin spinning={loadingAnnouncement}>
-          <div className="mig-platform grid grid-cols-1 space-y-5">
-            <div className="flex justify-between items-center">
-              <button
-                onClick={() => router.back()}
-                className="flex gap-1 items-center bg-transparent hover:opacity-70"
-              >
-                <ArrowLeftIconSvg size={20} />
-                <p>Kembali</p>
-              </button>
-
-              <ButtonSys
-                onClick={() => setShowDeleteModal(true)}
-                type={"default"}
-                color="danger"
-              >
-                <div className="flex gap-2 items-center">
-                  <DeleteOutlined color="#BF4A40" />
-                  <p>Hapus</p>
-                </div>
-              </ButtonSys>
-            </div>
-            <hr />
-            <h1 className="font-medium text-2xl">{dataAnnouncement?.title}</h1>
-            <p className="">
-              Oleh:{" "}
-              <span className="font-medium">
-                {dataAnnouncement?.user?.name}
-              </span>
-            </p>
-            {dataAnnouncement?.thumbnail_image?.link &&
-            dataAnnouncement?.thumbnail_image?.link !=
-              "staging/Announcement/mig-announce-logo.png" ? (
-              <img
-                src={generateStaticAssetUrl(
-                  dataAnnouncement?.thumbnail_image?.link
-                )}
-                alt={dataAnnouncement?.thumbnail_image?.description}
-                className="md:w-3/5 bg-cover object-cover rounded-md shadow-lg"
-              />
-            ) : (
-              <div
-                className="md:w-3/5 h-60 bg-backdrop rounded flex flex-col items-center 
-              justify-center py-10 px-6"
-              >
-                <img
-                  src="/mig.png"
-                  style={{ width: "10rem", mixBlendMode: "luminosity" }}
-                />
-              </div>
-            )}
-            <div className="py-4 parsed">
-              {dataAnnouncement?.text ? parse(dataAnnouncement?.text) : ""}
-            </div>
-          </div>
-        </Spin>
+        <AnnouncementMessageSection
+          announcementId={announcementId}
+          setShowDeleteModal={setShowDeleteModal}
+        />
       </div>
 
       <AccessControl hasPermission={ANNOUNCEMENT_DELETE}>
