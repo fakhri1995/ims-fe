60ac5714fb6ca4a455a2f3254fc64a3f04097bff
yasminadelia <yasminadelia@mitrasolusi.group>
2024-07-18 00:32:32 +0700

    feat `attendance`: refactor leave status badge into a component

---
+++ src/components/screen/attendance/leave/BadgeLeaveStatus.tsx
@@ -0,0 +1,32 @@
+export const BadgeLeaveStatus = ({ status }: { status: string }) => (
+  <div
+    className={`${
+      status == "1"
+        ? "bg-warning bg-opacity-10"
+        : status == "2"
+        ? "bg-primary100 bg-opacity-10"
+        : "bg-danger bg-opacity-10"
+    } py-1 px-4 max-w-max rounded-[5px] flex items-center gap-2`}
+  >
+    <span
+      className={`w-2 h-2 flex rounded-full ${
+        status == "1"
+          ? "bg-warning"
+          : status == "2"
+          ? "bg-primary100"
+          : "bg-danger"
+      } `}
+    ></span>
+    <p
+      className={`${
+        status == "1"
+          ? "text-warning"
+          : status == "2"
+          ? "text-primary100"
+          : "text-danger"
+      } mig-caption--medium`}
+    >
+      {status == "1" ? "Pending" : status == "2" ? "Accepted" : "Rejected"}
+    </p>
+  </div>
+);
