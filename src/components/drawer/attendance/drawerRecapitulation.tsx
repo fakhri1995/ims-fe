11390c873c8a803dcf624878d41111587054a901
Bakhtiar <bakhtiar@mitrasolusi.group>
2024-10-29 11:58:47 +0700

    update - recapitulation

--- src/components/drawer/attendance/drawerRecapitulation.tsx
+++ src/components/drawer/attendance/drawerRecapitulation.tsx
@@ -4,8 +4,10 @@ import {
   DatePicker,
   Form,
   InputNumber,
+  Radio,
   Row,
   Select,
+  Space,
   notification,
 } from "antd";
 import locale from "antd/lib/date-picker/locale/id_ID";
@@ -140,12 +142,15 @@ const DrawerRecapitulation = ({
       permissionWarningNotification("Mendapatkan", "Data Company");
     } else {
       setLoadingCompanyList(true);
-      fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/getCompanyClientList`, {
-        method: `GET`,
-        headers: {
-          Authorization: JSON.parse(dataToken),
-        },
-      })
+      fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/getCompanyClientList?with_mig=true`,
+        {
+          method: `GET`,
+          headers: {
+            Authorization: JSON.parse(dataToken),
+          },
+        }
+      )
         .then((res) => res.json())
         .then((res2) => {
           setDataCompanyList(res2.data);
@@ -189,56 +194,47 @@ const DrawerRecapitulation = ({
 
   const handleSubmit = (values) => {
     setLoadingSave(true);
-    let url = "";
-    let method = "POST";
-    let datapayload = {};
-    if (dataDefault) {
-      url = `updateEmployeeLeaveQuota`;
-      method = "PUT";
-      datapayload = {
-        id: dataDefault.id,
-        employee_id: values.employee_id,
-        leave_total: values.leave_quota,
-        start_period: moment(values.validity_period[0]).format("YYYY-MM-DD"),
-        end_period: moment(values.validity_period[1]).format("YYYY-MM-DD"),
-      };
-    } else {
-      url = "addEmployeeLeaveQuota";
-      method = "POST";
-      datapayload = {
-        employee_id: values.employee_id,
-        leave_total: values.leave_quota,
-        start_period: moment(values.validity_period[0]).format("YYYY-MM-DD"),
-        end_period: moment(values.validity_period[1]).format("YYYY-MM-DD"),
-      };
-    }
-    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/${url}`, {
-      method: method,
-      headers: {
-        Authorization: JSON.parse(dataToken),
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(datapayload),
-    })
-      .then((res) => res.json())
+    console.log("values ", values);
+    console.log("date form ", dateForm);
+    //${process.env.NEXT_PUBLIC_BACKEND_URL}/exportAttendanceRecap?employee_ids${values.employee_id}&company_id=${values.company_id}&start_date=${dateForm.start_date}&end_date=${dateForm.end_date}
+    fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/exportAttendanceRecap?start_date=2023-01-01`,
+      {
+        method: `GET`,
+        headers: {
+          Authorization: JSON.parse(dataToken),
+        },
+      }
+    )
+      .then((res) => res.blob())
       .then((res2) => {
-        if (res2.success) {
-          setLoadingSave(false);
-          instanceForm.resetFields();
-          resetParams();
-          onCancel();
-          fetchData();
-          notification["success"]({
-            message: `${dataDefault ? "Update" : " Add"} Leave Quota Success`,
-            duration: 3,
+        if (values.export_data == "PDF") {
+          var newBlob = new Blob([res2], {
+            type: "application/pdf",
+          });
+          const data = window.URL.createObjectURL(newBlob);
+          var link = document.createElement("a");
+          link.href = data;
+          link.download = `tes.pdf`;
+          link.click();
+          setTimeout(function () {
+            window.URL.revokeObjectURL(data);
           });
         } else {
-          setLoadingSave(false);
-          notification["error"]({
-            message: `${dataDefault ? "Update" : " Add"} Leave Quota Failed`,
-            duration: 3,
+          var newBlob = new Blob([res2], {
+            type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+          });
+          const data = window.URL.createObjectURL(newBlob);
+          var link = document.createElement("a");
+          link.href = data;
+          link.download = `tes.xlsx`;
+          link.click();
+          setTimeout(function () {
+            window.URL.revokeObjectURL(data);
           });
         }
+
+        setLoadingSave(false);
       });
   };
 
@@ -371,6 +367,12 @@ const DrawerRecapitulation = ({
                 label="Date Range"
                 name={"date_range"}
                 className="col-span-2"
+                rules={[
+                  {
+                    required: true,
+                    message: "Date Range is required",
+                  },
+                ]}
               >
                 <RangePicker className={"w-full"} onChange={onRangeChange} />
               </Form.Item>
@@ -387,14 +389,12 @@ const DrawerRecapitulation = ({
                   },
                 ]}
               >
-                <Checkbox.Group>
-                  <Row>
-                    <Checkbox value="PDF">PDF</Checkbox>
-                  </Row>
-                  <Row>
-                    <Checkbox value="Excel">Excel</Checkbox>
-                  </Row>
-                </Checkbox.Group>
+                <Radio.Group>
+                  <Space direction="vertical">
+                    <Radio value={"PDF"}>PDF</Radio>
+                    <Radio value={"Excel"}>Excel</Radio>
+                  </Space>
+                </Radio.Group>
               </Form.Item>
             </div>
           </div>
