a0b18e70b49d882b5bb81d50e07f3c7b0a2659c6
Bakhtiar <bakhtiar@mitrasolusi.group>
2025-08-25 16:40:43 +0700

    bug fixing recruitment ai

---
+++ src/components/screen/recruitment/searchcandidate/index.js
@@ -0,0 +1,136 @@
+import {
+  AppstoreOutlined,
+  CloseOutlined,
+  DownOutlined,
+} from "@ant-design/icons";
+import { Button, Dropdown, Input, Select, notification } from "antd";
+import { useRouter } from "next/router";
+
+import ButtonSys from "../../../button";
+import { SearchIconSvg, UserPlusIconSvg } from "../../../icon";
+
+const SearchCandidate = ({
+  searchingFilterRecruitments,
+  setSearchingFilterRecruitments,
+  setQueryParams,
+  onKeyPressHandler,
+  isAllowedToGetRecruitments,
+  queryParams,
+  isAllowedToGetRecruitmentRolesList,
+  setSelectedRoleId,
+  dataRoleList,
+  isAllowedToGetRecruitmentStagesList,
+  setSelectedStage,
+  dataStageList,
+  isAllowedToGetRecruitmentStatusesList,
+  setSelectedStatus,
+  dataStatusList,
+  onFilterRecruitments,
+}) => {
+  return (
+    <div className="flex flex-col gap-4 md:flex-row md:justify-between w-full md:items-center mb-4">
+      {/* Search by keyword (kata kunci) */}
+      <div className="w-full md:w-4/12">
+        <Input
+          defaultValue={searchingFilterRecruitments}
+          style={{ width: `100%` }}
+          placeholder="Kata Kunci.."
+          allowClear
+          onChange={(e) => {
+            setSearchingFilterRecruitments(e.target.value);
+            setQueryParams({ page: 1 });
+          }}
+          onKeyPress={onKeyPressHandler}
+          disabled={!isAllowedToGetRecruitments}
+        />
+      </div>
+
+      {/* Filter by role (dropdown) */}
+      <div className="w-full md:w-2/12">
+        <Select
+          defaultValue={queryParams.recruitment_role_id}
+          allowClear
+          name={`role`}
+          disabled={!isAllowedToGetRecruitmentRolesList}
+          placeholder="Semua Role"
+          style={{ width: `100%` }}
+          onChange={(value) => {
+            setQueryParams({ recruitment_role_id: value, page: 1 });
+            setSelectedRoleId(value);
+          }}
+        >
+          {dataRoleList.map((roles) => (
+            <Select.Option key={roles.id} value={roles.id}>
+              {roles.role}
+            </Select.Option>
+          ))}
+        </Select>
+      </div>
+
+      {/* Filter by stage */}
+      <div className="w-full md:w-2/12">
+        <Select
+          defaultValue={queryParams.recruitment_stage_id}
+          allowClear
+          name={`stage`}
+          disabled={!isAllowedToGetRecruitmentStagesList}
+          placeholder="Semua Stage"
+          style={{ width: `100%` }}
+          onChange={(value) => {
+            setQueryParams({ recruitment_stage_id: value, page: 1 });
+            setSelectedStage(value);
+          }}
+        >
+          {dataStageList.map((stage) => (
+            <Select.Option key={stage.id} value={stage.id}>
+              {stage.name}
+            </Select.Option>
+          ))}
+        </Select>
+      </div>
+
+      {/* Search by status (dropdown) */}
+      <div className="w-full md:w-2/12">
+        <Select
+          defaultValue={queryParams.recruitment_status_id}
+          allowClear
+          name={`status`}
+          disabled={!isAllowedToGetRecruitmentStatusesList}
+          placeholder="Semua Status"
+          style={{ width: `100%` }}
+          onChange={(value) => {
+            setQueryParams({ recruitment_status_id: value, page: 1 });
+            setSelectedStatus(value);
+          }}
+        >
+          {dataStatusList.map((status) => (
+            <Select.Option key={status.id} value={status.id}>
+              <div className="flex items-center">
+                <div
+                  className="rounded-full w-4 h-4 mr-2"
+                  style={{ backgroundColor: `${status.color}` }}
+                />
+                <p className="truncate">{status.name}</p>
+              </div>
+            </Select.Option>
+          ))}
+        </Select>
+      </div>
+
+      <div className="flex justify-end">
+        <ButtonSys
+          type={`primary`}
+          onClick={onFilterRecruitments}
+          disabled={!isAllowedToGetRecruitments}
+        >
+          <div className="flex flex-row space-x-2.5 w-full items-center">
+            <SearchIconSvg size={15} color={`#ffffff`} />
+            <p>Cari</p>
+          </div>
+        </ButtonSys>
+      </div>
+    </div>
+  );
+};
+
+export default SearchCandidate;
--- src/pages/admin/recruitment/cv/index.js
+++ src/pages/admin/recruitment/cv/index.js
@@ -19,6 +19,7 @@ import EducationInfoCard from "../../../../components/cards/resume/educationinfo
 import EvaluationCard from "../../../../components/cards/resume/evaluation/EvaluationCard";
 import ExperienceInfoCard from "../../../../components/cards/resume/experienceinfo/ExperienceInfoCard";
 import LanguageCard from "../../../../components/cards/resume/language/LanguageCard";
+import NeedToReviewCard from "../../../../components/cards/resume/needtoreview/NeedToReviewCard";
 import PersonalInfoCard from "../../../../components/cards/resume/personalinfo/PersonalInfoCard";
 import SkillCard from "../../../../components/cards/resume/skill/SkillCard";
 import ToolsCard from "../../../../components/cards/resume/tools/ToolsCard";
@@ -452,69 +453,11 @@ const CVDetail = ({ initProps, dataProfile, sidemenu }) => {
         </div>
         <div className={"mt-6 flex gap-[10px]"}>
           {dataRecruitments?.map((doc, idx) => (
-            <div
-              onClick={() => onChooseData(doc)}
-              className={`w-1/5 px-3 py-2.5 rounded-[5px] ${
-                doc.id == dataChoose?.id ? "bg-primary100" : "bg-white"
-              } border-1.5 ${
-                doc.id == dataChoose?.id
-                  ? "border-primary100"
-                  : "border-[#E6E6E6]"
-              } hover:cursor-pointer`}
-            >
-              <div className={"flex flex-col gap-2"}>
-                <p
-                  className={`${
-                    doc.id == dataChoose?.id ? "text-white" : "text-mono30"
-                  } text-sm leading-6 font-bold`}
-                >
-                  {doc.name}
-                </p>
-                <div className={"flex flex-col gap-1"}>
-                  <div className={"flex gap-1 items-start"}>
-                    <StarFillIconSvg color={"#E9C600"} />
-                    <p className={"text-[#E9C600] text-[10px] font-medium"}>
-                      {doc?.resume?.educations
-                        ? doc?.resume?.educations[
-                            doc?.resume?.educations.length - 1
-                          ].university
-                        : "-"}
-                    </p>
-                  </div>
-                  <div className={"flex gap-1 items-start"}>
-                    <RocketIconSvg
-                      color={doc.id == dataChoose?.id ? "white" : "#4D4D4D"}
-                    />
-                    {doc?.resume?.skills ? (
-                      <p
-                        className={`${
-                          doc.id == dataChoose?.id
-                            ? "text-white"
-                            : "text-mono30"
-                        } text-[10px] font-medium`}
-                      >
-                        {doc?.resume?.skills.map((skill, index) => (
-                          <span key={index}>
-                            {skill.name}
-                            {index < doc?.resume?.skills.length - 1 && ", "}
-                          </span>
-                        ))}
-                      </p>
-                    ) : (
-                      <p
-                        className={`${
-                          doc.id == dataChoose?.id
-                            ? "text-white"
-                            : "text-mono30"
-                        } text-[10px] font-medium`}
-                      >
-                        -
-                      </p>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
+            <NeedToReviewCard
+              onChooseData={onChooseData}
+              doc={doc}
+              dataChoose={dataChoose}
+            />
           ))}
         </div>
         <div className={"mt-6 flex justify-between mb-2"}>
