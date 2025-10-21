b5d5e287aff8d1b8176428b9d8f25f9821418da9
Bakhtiar <bakhtiar@mitrasolusi.group>
2025-10-21 10:51:12 +0700

    bug fixing

--- src/components/screen/attendance/staff/OldAttendanceStaffAktivitasSection.tsx
+++ src/components/screen/attendance/staff/OldAttendanceStaffAktivitasSection.tsx
@@ -355,7 +355,7 @@ export const AttendanceStaffAktivitasSection: FC<
   );
 
   const mOnRowItemClicked = useCallback(
-    (datum: typeof dataSource[0]) => {
+    (datum: (typeof dataSource)[0]) => {
       if (tabActiveKey === "2") {
         /** Only allow this click callback when user is on "Hari Ini" tab */
         return;
@@ -769,7 +769,7 @@ export const AttendanceStaffAktivitasSection: FC<
             <DataEmptyState caption="Belum ada aktivitas. Silakan masukkan aktivitas untuk hari ini" />
           )}
         >
-          <Table<typeof dataSource[0]>
+          <Table<(typeof dataSource)[0]>
             columns={tableColums}
             rowKey={(record) => record.id}
             dataSource={dataSource}
@@ -850,7 +850,7 @@ export const AttendanceStaffAktivitasSection: FC<
       );
     } else if (activeSubmenu == "aktivitas") {
       return (
-        <Table<typeof dataSource[0]>
+        <Table<(typeof dataSource)[0]>
           columns={TableTaskColumns}
           dataSource={displayDataTaskHistory}
           rowKey={(record) => record.id}
@@ -1211,6 +1211,7 @@ export const AttendanceStaffAktivitasSection: FC<
         <AttendanceStaffAktivitasDrawer
           visible={activityDrawerState.visible}
           action={activityDrawerState.openDrawerAs}
+          token={dataToken}
           activityFormId={activityDrawerState.selectedActivityFormId}
           onClose={() => dispatch({ type: "create", visible: false })}
         />
